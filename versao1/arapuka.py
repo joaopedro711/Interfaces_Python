f143c58d48b99661eb59d2636179c967c3dbfef6
João Pedro Silva <90197643+joaopedro711@users.noreply.github.com>
2023-08-17 22:22:44 -0300

    mapa
    está dando erro de runtime com o mapa e thread

---
+++ versao1/arapuka.py
@@ -0,0 +1,113 @@
+import PySimpleGUI as sg
+import eventos
+
+app_title = 'Arapuka v1'
+themes = sorted(list(sg.LOOK_AND_FEEL_TABLE.keys()))
+sg.ChangeLookAndFeel('DarkBlack')
+sg.SetOptions(font='any 11', auto_size_buttons=True, progress_meter_border_depth=0, border_width=1)
+
+
+# gui design
+def create_window():
+    # main tab
+    layout = [[sg.T('', size=(17, 1)), sg.Text('Arapuka', font='Any 50')],
+
+                [sg.Text('\n\n')],
+                [sg.Button('Log', size=(10, 2), pad=(10, 10))],
+                [sg.Text('Estados:')],
+                # botões dos estados
+                [sg.Button('Dormente', size=(10, 2), pad=(10, 10)), sg.Button('Vigilia', size=(10, 2), pad=(10, 10)), sg.Button('Alerta 1', size=(10, 2), pad=(10, 10)), sg.Button('Alerta 2', size=(10, 2), pad=(10, 10)), sg.Button('Suspeito', size=(10, 2), pad=(10, 10))], 
+                
+                
+                [sg.Text('\n\nAções:')],
+                #botoes das açoes extras
+                [sg.Button('Status', size=(10, 2), pad=(10, 20)), sg.Button('Rd n', size=(10, 2), pad=(10, 20)), sg.Button('Rd n_m', size=(10, 2), pad=(10, 20)), sg.Button('Apagar Memoria', size=(10, 2), pad=(10, 20)), sg.Button('E-mail', size=(10, 2), pad=(10, 20))],
+                [sg.Button('Resete', size=(10, 2), pad=(10, 20)), sg.Button('Set RTC', size=(10, 2), pad=(10, 20))],   
+                                
+            ]
+
+
+
+    # window
+    return sg.Window(title=app_title, layout=layout, size=(600, 550), icon='arapuka.ico')
+
+window = create_window()
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED:   
+        break
+
+    elif event == 'Log':
+        window.hide()
+        eventos.log()
+        window.un_hide()
+
+    #Eventos dos Estados
+    elif event == 'Dormente':
+        window.hide()
+        eventos.dormente()
+        window.un_hide()
+
+    elif event == 'Vigilia':
+        window.hide()
+        eventos.vigilia() 
+        window.un_hide()  
+
+    elif event == 'Alerta 1':
+        window.hide()
+        eventos.alerta_1()
+        window.un_hide()
+
+    elif event == 'Alerta 2':
+        window.hide()
+        eventos.alerta_2() 
+        window.un_hide()
+
+    elif event == 'Suspeito':
+        window.hide()
+        eventos.suspeito()
+        window.un_hide()
+
+    #Eventos das Ações
+    elif event == 'Status':
+        window.hide()
+        eventos.status()   
+        window.un_hide() 
+
+    elif event == 'Rd n':
+        window.hide()
+        eventos.rd_n()
+        window.un_hide()
+
+    elif event == 'Rd n_m':
+        window.hide()
+        eventos.rd_n_m()  
+        window.un_hide() 
+
+    elif event == 'Apagar Memoria':
+        window.hide()
+        eventos.apagar_memoria()
+        window.un_hide()  
+
+    elif event == 'E-mail':
+        window.hide()
+        eventos.email()
+        window.un_hide()
+
+    elif event == 'Resete':
+        window.hide()
+        eventos.resete()
+        window.un_hide()
+
+    elif event == 'Set RTC':
+        window.hide()
+        eventos.rtc()
+        window.un_hide()     
+
+#precisa entrar aqui, porém nao esta entrando
+if eventos.update_thread is not None and eventos.update_thread.is_alive():
+    eventos.update_thread.join()
+
+window.close()   
---
+++ versao1/eventos.py
@@ -0,0 +1,247 @@
+import PySimpleGUI as sg
+import threading
+import datetime
+import queue
+import requisicoes
+import auxiliares
+
+# global update_thread
+# update_thread = None
+
+def log():
+    #cria console
+    new_window = auxiliares.console_get("Log")
+
+    #Requisição GET
+    update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+    update_thread.start()
+    
+    while True:
+        event, values = new_window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Fechar':
+            # if update_thread is not None and update_thread.is_alive():
+            #     update_thread.join()
+            new_window.close()
+            break
+        elif event == 'Mostrar Mapa':
+            # sg.popup(f"{auxiliares.localization}")
+            if auxiliares.localization != None:     #Se for diferente de vazia tenta mostrar o mapa
+                latitude, longitude = auxiliares.extrair_latitude_longitude(auxiliares.localization)
+                if (latitude and longitude) != False:
+                    try:
+                        latitude = float(latitude)
+                        longitude = float(longitude)
+
+                        thread = threading.Thread(target=auxiliares.exibir_mapa, args=(latitude, longitude))
+                        thread.start()
+
+                        # Espera a thread terminar antes de abrir a nova janela
+                        thread.join()
+
+                        try:
+                            update_type, update_data = auxiliares.update_queue.get_nowait()
+                            if update_type == 'show_map':
+                                auxiliares.criar_janela_mapa(update_data)
+                            elif update_type == 'error':
+                                sg.popup_error(update_data)
+                        except queue.Empty:
+                            pass
+
+                    except ValueError:
+                        sg.popup_error('Digite valores válidos para Latitude e Longitude.')
+            else:            
+                sg.popup_error('Ainda Não possui dados da última localização.',title="Map Error", icon='arapuka.ico')            
+
+
+# Funções para cada botão dos Estados
+def dormente():
+    ###################################################################################################     
+    #envia a requisição POST
+    post_result = requisicoes.post_comando("#DMT#")
+
+    #cria console
+    new_window = auxiliares.console_duo("Dormente")
+
+    print(post_result, file=new_window["OutputPost"])   #Coloca se conseguiu enviar a mensagem ou não
+    print("Estado Dormente", file=new_window["OutputPost"])
+    
+    #Requisição GET
+    update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+    update_thread.start()
+    
+    while True:
+        event, values = new_window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Fechar':
+            # if update_thread is not None and update_thread.is_alive():
+            #     update_thread.join()
+            new_window.close()
+            break
+    ###################################################################################################     
+
+       
+
+def vigilia():
+    ################################################################################################### 
+    #envia a requisição POST
+    post_result = requisicoes.post_comando("#VIG#")
+
+    #cria console
+    new_window = auxiliares.console_duo("Vigilia")
+
+    print(post_result, file=new_window["OutputPost"])   #Coloca se conseguiu enviar a mensagem ou não
+    print("Estado Vigilia", file=new_window["OutputPost"])
+
+    #Requisição GET
+    update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+    update_thread.start()
+   
+    while True:
+        event, values = new_window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Fechar':
+            # if update_thread is not None and update_thread.is_alive():
+            #     update_thread.join()
+            new_window.close()
+            break
+    ###################################################################################################     
+
+def alerta_1():
+    sg.popup('Botão 3 pressionado')
+
+def alerta_2():
+    sg.popup('Botão 4 pressionado')
+
+def suspeito():
+    sg.popup('Botão 5 pressionado')
+
+# Funções para cada botão das Ações
+def status():
+    sg.popup('Botão 6 pressionado')
+
+def rd_n():
+    window = auxiliares.rd_n()
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            break
+        elif event == 'Enviar':
+            window.close()
+            number_n = int(values['combo'])
+                            #chama a função que envia o POST 
+    ###################################################################################################            
+            #envia a requisição POST
+            post_result = requisicoes.post_comando(f"#RD {number_n}#")
+
+            #cria console
+            new_window = auxiliares.console_duo("Leitura n Registros")
+
+            print(post_result, file=new_window["OutputPost"])   #Coloca se conseguiu enviar a mensagem ou não
+            print(f"Pedido de leitura de {number_n} Registros enviado!", file=new_window["OutputPost"])
+
+            #Requisição GET
+            update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+            update_thread.start()
+
+            while True:
+                event, values = new_window.read()
+                if event == sg.WINDOW_CLOSED or event == 'Fechar':
+                    # if update_thread is not None and update_thread.is_alive():
+                    #     update_thread.join()
+                    new_window.close()
+                    break
+    ###################################################################################################   
+
+    window.close()
+
+def rd_n_m():
+    window = auxiliares.rd_n_m()
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            break
+        elif event == 'Enviar':
+            
+            number_n = int(values['combo1'])
+            number_m = int(values['combo2'])
+            #chama a função que envia o POST 
+    ###################################################################################################  
+            if(auxiliares.is_valid_RD_n_m(number_n, number_m)==True):    
+                window.close()      
+                #envia a requisição POST
+                post_result = requisicoes.post_comando(f"#RD {number_n} {number_m}#")
+
+                #cria console
+                new_window = auxiliares.console_duo("Leitura n até m Registros")
+
+                print(post_result, file=new_window["OutputPost"])   #Coloca se conseguiu enviar a mensagem ou não
+                print(f"Pedido de leitura do {number_n}° até o {number_m}° Registro enviado!", file=new_window["OutputPost"])
+
+                #Requisição GET
+                update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+                update_thread.start()
+
+                while True:
+                    event, values = new_window.read()
+                    if event == sg.WINDOW_CLOSED or event == 'Fechar':
+                        # if update_thread is not None and update_thread.is_alive():
+                        #     update_thread.join()
+                        new_window.close()
+                        break
+    ################################################################################################### 
+            else:
+                sg.popup_error('O segundo numero precisa ser maior que o primeiro.',title="Números inválidos", icon='arapuka.ico') 
+
+    window.close()
+
+def apagar_memoria():
+    sg.popup('Botão 9 pressionado')
+
+def email():
+    window = auxiliares.email_layout()
+    while True:
+        event, values = window.read()
+
+        if event == sg.WINDOW_CLOSED:
+            window.close() 
+            break
+        elif event == 'Enviar':
+            email = values['email']
+            if auxiliares.is_valid_email(email):
+                window.close()
+                #chama a função que envia o POST com o email e mostra o resultado no console
+    ###################################################################################################            
+                #envia a requisição POST
+                post_result = requisicoes.post_comando(f"#MAIL#{email}")
+
+                #cria console
+                new_window = auxiliares.console_duo("Envio de E-mail")
+
+                print(post_result, file=new_window["OutputPost"])   #Coloca se conseguiu enviar a mensagem ou não
+                print("Pedido de E-mail enviado!", file=new_window["OutputPost"])
+    
+                #Requisição GET
+                update_thread = threading.Thread(target=auxiliares.update_console, args=(new_window['OutputGet'], requisicoes.url_resposta))
+                update_thread.start()
+
+                while True:
+                    event, values = new_window.read()
+                    if event == sg.WINDOW_CLOSED or event == 'Fechar':
+                        # if update_thread is not None and update_thread.is_alive():
+                        #     update_thread.join()
+                        new_window.close()
+                        break
+    ###################################################################################################                 
+            else:
+                sg.popup_error('E-mail inválido. Por favor, digite novamente.', title="E-mail inválido", icon='arapuka.ico')
+    window.close()        
+
+def resete():
+    sg.popup('Botão 11 pressionado')
+
+def rtc():
+    agora = datetime.datetime.now()
+    data_hora_formatada = agora.strftime("%d/%m/%y %H:%M:%S")
+    #envia a requisição POST
+    post_result = requisicoes.post_comando(f"#RTC {data_hora_formatada}#")
+    sg.popup(f"{post_result}\nReal Time Clock: {data_hora_formatada}", title="Data e Hora", icon='arapuka.ico')
